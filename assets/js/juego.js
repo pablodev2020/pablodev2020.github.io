3034ac617d077f1d625675e995565f3d7ccd03ea
PabloMezaDev <78183501+pablodev2020@users.noreply.github.com>
2022-05-11 00:18:39 -0300

    Update juego.js

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -128,7 +128,7 @@ const miModulo=(()=>{
                   }) 
                     const sonido=()=>{
                         let sonidito = new Audio();
-                        sonidito.src="http://chatero.epizy.com/aplausos.mp3";
+                        sonidito.src="assets/music/aplausos.mp3";
                         sonidito.play();  
                     }
                   sonido();
